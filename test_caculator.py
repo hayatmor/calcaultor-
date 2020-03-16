7c920a804f8c7ac2ed6822f4975597804a79c52a
mor hayat <mor@ti.com>
2020-03-16 14:48:48 +0200

    fix flake8 errors

--- test_caculator.py
+++ test_caculator.py
@@ -1,10 +1,9 @@
 import calculator
 
+
 class TestCalcualtor:
-
     def test_add(self):
-        assert 4 == calculator.add(2,2)
-
+        assert 4 == calculator.add(2, 2)
 
     def test_sub(self):
-        assert 2 == calculator.subtract(4,2)
+        assert 2 == calculator.subtract(4, 2)
